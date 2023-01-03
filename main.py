6d842b421617d651db3bd810e809ba021f1667a2
jboixados <jaime.boixados@meritis.fr>
2023-01-03 20:31:46 +0100

    Refactoring + language-plot

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from experience_handler import ExperienceHandler
 from utils import json_reader
 
@@ -5,6 +7,8 @@ from flask import Flask, render_template, request, jsonify
 
 
 app = Flask(__name__)
+user_info: dict = {}
+exp_handler: Optional[ExperienceHandler] = None
 
 
 @app.route("/")
@@ -17,8 +21,14 @@ def experience_filter():
     return jsonify(exp_handler.apply_filter(request.form["filter"]))
 
 
-if __name__ == "__main__":
+def create_app():
+    global user_info
+    global exp_handler
     user_info = json_reader()
     exp_handler = ExperienceHandler(user_info["experiences"])
     app.debug = True
     app.run()  # go to http://localhost:5000/ to view the page.
+
+
+if __name__ == "__main__":
+    create_app()
