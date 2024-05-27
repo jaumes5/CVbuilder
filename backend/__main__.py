04154da79c1343a8ab0e0f816bf5759629fe99bf
jaumes5 <jaumes524@gmail.com>
2024-05-27 22:51:49 +0200

    Improve structure

---
+++ backend/__main__.py
@@ -0,0 +1,24 @@
+import argparse
+
+from backend.router import create_app
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser(description="Run the Flask app")
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=3000,
+        help="Port to run the app on (default 3000)",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = arg_parser()
+    create_app(args.port)
+
+
+if __name__ == "__main__":
+    main()
--- backend/router.py
+++ backend/router.py
@@ -1,4 +1,3 @@
-import argparse
 import logging
 from typing import Optional
 
@@ -8,7 +7,7 @@ from utils import json_reader
 from flask import Flask, render_template, request, jsonify
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 user_info: dict = {}
 exp_handler: Optional[ExperienceHandler] = None
 
@@ -30,18 +29,6 @@ def experience_filter():
     return jsonify(exp_handler.apply_filter(request.form["filter"]))
 
 
-def arg_parser():
-    parser = argparse.ArgumentParser(description="Run the Flask app")
-    parser.add_argument(
-        "-p",
-        "--port",
-        type=int,
-        default=3000,
-        help="Port to run the app on (default 3000)",
-    )
-    return parser.parse_args()
-
-
 def create_app(port: int = 3000):
     global user_info
     global exp_handler
@@ -50,12 +37,3 @@ def create_app(port: int = 3000):
     exp_handler = ExperienceHandler(user_info["experiences"])
     app.debug = False
     app.run(host="0.0.0.0", port=port)
-
-
-def main():
-    args = arg_parser()
-    create_app(args.port)
-
-
-if __name__ == "__main__":
-    main()
